026790f7f41ddb6b60c500a0de8f956a2927b6d8
jim-clark <email@jim-clark.com>
2020-05-01 14:52:47 -0700

    Render player turn

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ let winner;   // 1 = Player 1; -1 = Player 2; 'T' = tie; null = no winner/tie
 const markerEls = Array.from(document.querySelectorAll('#markers > div'));
 // OR
 // const markerEls = [...document.querySelectorAll('#markers > div')];
+const msgEl = document.getElementById('msg');
 
 /*----- event listeners -----*/
 document.getElementById('markers')
@@ -46,6 +47,13 @@ function render() {
       div.style.backgroundColor = playerLookup[cell];
     });
   });
+  // Render turn or winner message
+  if (winner) {
+
+  } else {
+    // Render whose turn
+    msgEl.innerHTML = `<span style="color: ${playerLookup[turn]}">${playerLookup[turn].toUpperCase()}'s</span> Turn`;
+  }
 }
 
 function handleClick(evt) {
